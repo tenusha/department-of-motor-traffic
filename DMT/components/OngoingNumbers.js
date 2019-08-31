429b97c847a7a4be50dde61787cf337891d6cc7e
Aravinda1995 <aravindakulasooriya1995@gmail.com>
2019-08-31 21:22:13 +0530

    3rd Commit added images

--- DMT/components/OngoingNumbers.js
+++ DMT/components/OngoingNumbers.js
@@ -3,7 +3,21 @@ import {Image, Text, FlatList, Dimensions, View, StyleSheet} from 'react-native'
 import AppHeader from "./commons/AppHeader";
 
 const data = [
-    {key: 'A', image: ''}, {key: 'B',  image: ''}, {key: 'B',  image: ''}, {key: 'C',  image: ''}, {key: 'D',  image: ''}, {key: 'E',  image: ''}, {key: 'F',  image: ''}, {key: 'G',  image: ''}, {key: 'H',  image: ''}, {key: 'I',  image: ''}
+    {key: 'A', img: require('../assets/OngoingNumberIcons/carIcon.png')},
+    {key: 'B',  img: require('../assets/OngoingNumberIcons/bigTruckIcon.png')},
+    {key: 'B',  img: require('../assets/OngoingNumberIcons/buldozerIcon.png')},
+    {key: 'C', img: require('../assets/OngoingNumberIcons/busIcon.png')},
+    {key: 'D', img: require('../assets/OngoingNumberIcons/cementIcon.png')},
+    {key: 'E',  img: require('../assets/OngoingNumberIcons/commercialIcon.png')},
+    {key: 'F',  img: require('../assets/OngoingNumberIcons/excavatorIcon.png')},
+    {key: 'G',  img: require('../assets/OngoingNumberIcons/forkLiftIcon.png')},
+    {key: 'H',  img: require('../assets/OngoingNumberIcons/lorryIcon.png')},
+    {key: 'I',  img: require('../assets/OngoingNumberIcons/panzerIcon.png')},
+    {key: 'I',  img: require('../assets/OngoingNumberIcons/primeMoverIcon.png')},
+    {key: 'I',  img: require('../assets/OngoingNumberIcons/singleCabIcon.png')},
+    {key: 'I',  img: require('../assets/OngoingNumberIcons/tractorTrailerIcon.png')},
+    {key: 'I',  img: require('../assets/OngoingNumberIcons/trailerIcon.png')}
+
 ];
 
 const formatData = (data, numOfColumns) => {
@@ -32,9 +46,9 @@ export default class OngoingNumbers extends React.Component {
       return (
           <>
             <View style={styles.item}>
-                <Image source={require('../assets/OngoingNumberIcons/carIcon.png')} style={styles.itemImage}/>
+                <Image source={item.img} style={styles.itemImage}/>
                 <Text style={styles.itemText}>{item.key}</Text>
-                <Text>The current number is:</Text>
+                <Text>The next ongoing number is:</Text>
                 <Text style={styles.itemLicenceNo}>{"XC-345543"}</Text>
             </View>
           </>
@@ -47,6 +61,7 @@ export default class OngoingNumbers extends React.Component {
             <>
                 <AppHeader {...this.props} title={'Ongoing numbers'}/>
                 <FlatList
+                    contentContainerStyle={{paddingBottom:30}}
                     data={formatData(data,2)}
                     style={styles.container}
                     renderItem={this.renderItem}
@@ -67,12 +82,15 @@ const styles = StyleSheet.create({
        alignItems: 'center',
        justifyContent: 'center',
        flex: 1,
-       margin: 1,
+       margin: 2,
+       borderWidth: 1,
+       borderColor: '#E24244',
        height: Dimensions.get('window').width / 2,
    },
    itemImage: {
        width:  Dimensions.get('window').width / 2 - 50,
-       height:  Dimensions.get('window').width / 2 - 95
+       height:  Dimensions.get('window').width / 2 - 115,
+       resizeMode: 'contain'
    },
    itemInvisible: {
         backgroundColor: 'transparent'
@@ -80,7 +98,7 @@ const styles = StyleSheet.create({
    itemText: {
        color: '#000',
        fontWeight: 'bold',
-       fontSize: 20
+       fontSize: 25
    },
    itemLicenceNo: {
        color: '#E24244',
