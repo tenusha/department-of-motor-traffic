6f4cf4ff6252f3fa684da7924eaffbfba2faba32
vimukthi <tenushamadhushan@gmail.com>
2019-09-01 17:15:32 +0530

    style changes

--- DMT/components/PaymentGateway.js
+++ DMT/components/PaymentGateway.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {ScrollView, View, Text} from 'react-native';
-import AppHeader from "./commons/AppHeader";
+import {ScrollView, View, Text} from 'react-native'
+import {Header, Icon} from "react-native-elements"
 
 
 export default class PaymentGateway extends React.Component {
@@ -12,14 +12,24 @@ export default class PaymentGateway extends React.Component {
     render() {
         const {navigate} = this.props.navigation;
         return (
-            <>
-                <AppHeader {...this.props} title={'Payment Gateway'}/>
+            <View>
                 <ScrollView>
-                    <View>
+                    <Header
+                        containerStyle={{
+                            backgroundColor: 'white',
+                            borderBottomColor: 'white'
+                        }}
+                        leftComponent={<Icon
+                            name={'arrow-back'}
+                            color={'black'}
+                            onPress={() => this.props.navigation.navigate("FineManagement")}
+                        />}
+                    />
+                    <View style={{marginLeft: 20}}>
                         <Text>Payment Gateway</Text>
                     </View>
                 </ScrollView>
-            </>
+            </View>
         );
     }
 }
