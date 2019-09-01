8d2d56b066ebd18af7900cc82536fa7ecc67b2f5
Ranmal-Dewage <ranmal.b.dewage@gmail.com>
2019-09-01 11:44:17 +0530

    Fine Management Phase and Vehicle Details

--- DMT/components/VehicleDetails.js
+++ DMT/components/VehicleDetails.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import AppHeader from "./commons/AppHeader";
 import CustomTextField from "./commons/CustomTextField";
-import {Button} from "react-native-elements";
-import CustomTextField2 from "./commons/CustomTextField"
+import {Button, Card} from "react-native-elements";
 import configs from "../config";
 
 export default class VehicleDetails extends React.Component {
@@ -13,36 +12,113 @@ export default class VehicleDetails extends React.Component {
             <Image source={require('../assets/icons/vehicle_info.png')} style={{width: 24, height: 24}}/>
         )
     };
+    state = {
+        display: false
+    }
 
+    handleDisplay = () =>{
+        this.setState({display: !this.state.display})
+    }
     render() {
         const {navigate} = this.props.navigation;
         return (
             <>
                 <AppHeader {...this.props} title={'Vehicle Details'}/>
-                {/*<CustomTextField2 style={styles.materialMessageTextbox}*/}
-                {/*                 placeholder={'e.g: KA-1010, 15-3456'}*/}
-                {/*                 label={'Vehicle Number :'}*/}
-                {/*                 value=''*/}
-                {/*                />*/}
-                <View style={{alignItems: 'flex-end'}}>
-                    <Button
-                        title="Add new vehicle"
-                        type="outline"
-                        containerStyle={{width: 150, height: 36, marginTop: 20, marginRight: 10}}
-                        buttonStyle={{borderColor: configs.theme}}
-                        titleStyle={{
-                            color: configs.theme
+                <ScrollView>
+                    <CustomTextField style={styles.materialMessageTextbox}
+                                     placeholder={'e.g: KA-1010, 15-3456'}
+                                     label={'Vehicle Number :'}
+                                     value=''
+                    />
+                    <View style={{alignItems: 'flex-end'}}>
+                        <Button
+                            title="Get Details"
+                            onPress={this.handleDisplay}
+                            containerStyle={{width: 150, height: 36, marginTop: 20, marginRight: 10}}
+                            buttonStyle={{backgroundColor: configs.buttonCol}}
+                            titleStyle={{
+                                color: "#FFFFFF"
+                            }}
+                        />
+                    </View>
+                    <View
+                        style={{
+                            borderBottomColor: '#D3D3D3',
+                            borderBottomWidth: 1,
+                            margin: 10,
+                            marginTop: 25
                         }}
                     />
-                </View>
-                <View
-                    style={{
-                        borderBottomColor: '#D3D3D3',
-                        borderBottomWidth: 1,
-                        margin: 10,
-                        marginTop: 25
-                    }}
-                />
+
+                    {this.state.display && <Card
+                        title='Vehicle Details'
+                        titleStyle={{fontSize: 18}}>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Vehicle Owner</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: T.M.Guruge</Text></View>
+                        </View>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Engine Number</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: H567685</Text></View>
+                        </View>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Class</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: Motor Car</Text></View>
+                        </View>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Manufacturer</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: Toyota</Text></View>
+                        </View>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Model</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: Axia RX</Text></View>
+                        </View>
+                        <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                            <View style={{flex: 1, alignSelf: 'stretch'}}><Text>Year</Text></View>
+                            <View style={{
+                                flex: 1,
+                                alignSelf: 'stretch'
+                            }}><Text
+                                style={{
+                                    fontWeight: "bold"
+                                }}>: 2018</Text></View>
+                        </View>
+                    </Card>}
+
+                    <View style={{flex: 1, alignSelf: 'stretch', flexDirection: 'row', marginBottom: 25}}>
+                    </View>
+
+                </ScrollView>
             </>
         );
     }
