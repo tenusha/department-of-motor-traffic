cc148c340fec5e2ac53b803f375f0e0f9865bf8c
Aravinda1995 <aravindakulasooriya1995@gmail.com>
2019-09-14 11:54:39 +0530

    with backend

---
+++ services/spring/uee-user-backend/src/main/java/lk/sliit/uee/domain/UserDTO.java
@@ -0,0 +1,45 @@
+/**
+ * 
+ */
+package lk.sliit.uee.domain;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author Vimukthi Rajapaksha
+ * @date Aug 28, 2019
+ */
+public class UserDTO {
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String password;
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ services/spring/uee-user-backend/src/main/java/lk/sliit/uee/domain/UserRegDTO.java
@@ -0,0 +1,109 @@
+/**
+ * 
+ */
+package lk.sliit.uee.domain;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author Vimukthi Rajapaksha
+ * @date Aug 28, 2019
+ */
+public class UserRegDTO {
+	@NotBlank
+	private String userId;
+	@NotBlank
+	private String firstName;
+	@NotBlank
+	private String secondName;
+	@NotBlank
+	private String email;
+	@NotBlank
+	private String mobile;
+	private String imagePath;
+
+	/**
+	 * @return the userId
+	 */
+	public String getUserId() {
+		return userId;
+	}
+
+	/**
+	 * @param userId the userId to set
+	 */
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the secondName
+	 */
+	public String getSecondName() {
+		return secondName;
+	}
+
+	/**
+	 * @param secondName the secondName to set
+	 */
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the mobile
+	 */
+	public String getMobile() {
+		return mobile;
+	}
+
+	/**
+	 * @param mobile the mobile to set
+	 */
+	public void setMobile(String mobile) {
+		this.mobile = mobile;
+	}
+
+	/**
+	 * @return the imagePath
+	 */
+	public String getImagePath() {
+		return imagePath;
+	}
+
+	/**
+	 * @param imagePath the imagePath to set
+	 */
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+
+}
