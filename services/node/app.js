6eee9246b73a3784b67c65af061ff604c77357f8
Ranmal-Dewage <ranmal.b.dewage@gmail.com>
2019-09-14 22:08:36 +0530

    Vehicle Details Test Backend

--- services/node/app.js
+++ services/node/app.js
@@ -3,6 +3,22 @@ const express = require('express')
 const app = express()
 const revenue_license = require('./routers/revenue_license')
 const user_vehicles = require('./routers/user_vehicles')
+const slvehicle_details = require('./routers/slvehicle_details')
+const mongoose = require("mongoose");
+
+// DB Connection
+mongoose.connect("mongodb://localhost:27017/DMT", { useUnifiedTopology: true  })
+
+let db = mongoose.connection;
+
+db.on("error", (err) => {
+    console.log(err);
+});
+
+db.once("open", () => {
+    console.log("Connected to MongoDB");
+});
+
 
 app.use(express.json());
 app.use(function (req, res, next) {
@@ -14,6 +30,7 @@ app.use(function (req, res, next) {
 
 app.use("/revenueLicense",revenue_license)
 app.use("/users",user_vehicles)
+app.use("/slvehicles",slvehicle_details)
 
 app.listen(3001, err => {
     if (err) {
